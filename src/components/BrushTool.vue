4fad6729dc140a2aa97c40824177856d43faabbc
Kurtzmusch <kurtzmusch@gmail.com>
2021-12-18 10:01:40 -0300

    added a descriptio/guide for some tools

--- src/components/BrushTool.vue
+++ src/components/BrushTool.vue
@@ -17,6 +17,14 @@
           <input id='star-amount' class='form-control' type="number" v-model.number='galaxyEditor.brushOptions.starAmount'/>
         </div>
       </div>
+
+      <br/>
+
+      <div class='row'>
+        <div class='col'>
+          <p>Click to place stars. If <i class='text-info'>Star Amount</i> is 1, the star will be placed precisely at the cursor position, otherwise, multiple stars will be placed within <i class='text-info'>Brush Radius</i></p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
